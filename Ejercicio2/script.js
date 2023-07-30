8036506748fd38e7151c0673dbe1bc2d1f12f3aa
Jonitha42 <yonipardo4@gmail.com>
2023-07-30 19:46:21 -0300

    Ejercicio3

--- Ejercicio2/script.js
+++ Ejercicio2/script.js
@@ -29,4 +29,3 @@ perimetroID.innerHTML = perimetroID.innerHTML + perimetro;
 
 
 
-
---
+++ Ejercicio3/script.js
@@ -0,0 +1,6 @@
+// Escriba un programa en donde se pida la edad del usuario. Si el usuario es mayor de
+// edad se debe mostrar un mensaje indicándolo.
+
+let edad = prompt("Ingrese su edad: ");
+
+edad >= 18 ? alert("El usuario es mayor de edad") : alert("El usuario no es mayor de edad")
